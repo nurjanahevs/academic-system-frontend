88320d6b150f0c0ac179f0fdd7635a7912a1de73
Diar J R <diar.student@gmail.com>
2022-01-10 15:02:37 +0700

    update login development

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../_services/auth-service.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  form: any = {
+    email: null,
+    password: null,
+    role: null,
+  };
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  token: string | null = '';
+
+  constructor(
+    private authService: AuthServiceService,
+    private tokenStorage: TokenStorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorage.getToken();
+
+    if (this.isLoggedIn) {
+      this.router.navigate(['dashboard']);
+    }
+  }
+
+  selectedvalue(role: any) {
+    this.form.role = role.target.value;
+  }
+
+  onSubmit(): void {
+    const { email, password, role } = this.form;
+    console.log(email, password, role);
+    this.authService.login(email, password, role).subscribe(
+      (res) => {
+        const token = res.token;
+        this.token = token;
+        this.tokenStorage.saveToken(token);
+        this.tokenStorage.saveUser(res);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.errorMessage = 'Login Success! wait 2 second...';
+        console.log(res);
+        setTimeout(() => {
+          this.router.navigate(['dashboard']);
+        }, 2000);
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+}
